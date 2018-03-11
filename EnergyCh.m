e4f0d996928317257eab597e2d94c4f09ba5e187
pradeep nazareth <pradeep.nitk2017@gmail.com>
2018-03-11 14:36:24 +0530

    CH Energy Model initial version

--- EnergyCh.m
+++ EnergyCh.m
@@ -1,9 +1,43 @@
-function [outputArg1,outputArg2] = EnergyCh(ClusterModel,Round)
-CH=ClusterModel.CH;
+function ClusterModel = EnergyCh(ClusterModel,Round)
+
+ClusterHead=ClusterModel.CH;
 Nodes= ClusterModel.Nodes;
 Network=ClusterModel.Network;
 
-d0=sqrt(
+d0=sqrt(Network.Energy.FreeSpace/Network.Energy.MultiPath);
+NoCh=length(ClusterHead.no);
+
+
+% Energy parameters 
+
+EMP=Network.Energy.MultiPath;
+EFS=Network.Energy.FreeSpace;
+ETX=Network.Energy.TxEnergy;
+ERX=Network.Energy.RxEnergy;
+EDA=Network.Energy.AggEnergy;
+PacketSize=Round.ChSinkPkt;
+
+for i=1:NoCh
+ChNo=ClusterHead.no(i);
+
+distance = ClusterHead.distancetoBS(i);
+Energy=Nodes.node(ChNo).Energy;
+% Energy consumed by CH during data transmission to BS
+if (distance >d0)
+Nodes.node(ChNo).Energy=Energy -((ETX+EDA) * PacketSize + EMP * PacketSize * (distance ^ 4));
+else
+Nodes.node(ChNo).Energy=Energy -((ETX+EDA) * PacketSize + EFS * PacketSize * (distance ^ 2));
+end
+% Energy consumed by CH in receivinh data from cluster member
+Nodes.node(chNo).Energy = Nodes.node(chNo).Energy - PacketSize*ERX *round(Nodes.NumNodes/ClusterModel.NumCluster);
+end
+ClusterMode.Nodes=Nodes;
+end
+
+
+
+
+
 
 end
 
